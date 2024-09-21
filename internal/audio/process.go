719f40e0094c865b6d8631c7c74df0e3c9b2c403
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-09-21 23:33:34 +0900

    settup noise reduction (add attenuation)

--- internal/audio/process.go
+++ internal/audio/process.go
@@ -1,9 +1,12 @@
 package audio
 
+import "math"
+
 
 
 type AudioProcessor struct {
 	noiseThreshold 	float64
+	attenuation		float64
 	sampleRate 		int
 	numChannels 	int
 }
@@ -11,32 +14,34 @@ type AudioProcessor struct {
 func NewAudioProcessor(sampleRate, numChannels int) *AudioProcessor {
 	return &AudioProcessor{
 		noiseThreshold: 0.05, // moderate threshold
+		attenuation: 0.5, // reduce noise by 50%
 		sampleRate: sampleRate,
 		numChannels: numChannels,
 	}
 }
 
 func (ap *AudioProcessor) ReduceNoise(input []byte) []byte {
-	return input
-	// output := make([]byte, len(input))
-	// samplesPerChannel := len(input) / (2 * ap.numChannels) // 16-bit samples
+	
+	output := make([]byte, len(input))
+	samplesPerChannel := len(input) / (2 * ap.numChannels) // 16-bit samples
 
-	// for i := 0; i < samplesPerChannel; i++ {
-	// 	for ch := 0; ch < ap.numChannels; ch++ {
-	// 		idx := (i*ap.numChannels + ch) * 2
-	// 		sample := int16(input[idx]) | (int16(input[idx+1]) << 8)
+	for i := 0; i < samplesPerChannel; i++ {
+		for ch := 0; ch < ap.numChannels; ch++ {
+			idx := (i*ap.numChannels + ch) * 2
 
-	// 		// simple noise gate
-	// 		if math.Abs(float64(sample)) < ap.noiseThreshold*32767 { // 32767 = max value for 16-bit audio
-	// 			sample = 0
-	// 		}
+			sample := int16(input[idx]) | (int16(input[idx+1]) << 8)
 
-	// 		// convert back to bytes
-	// 		output[idx] = byte(sample)
-	// 		output[idx+1] = byte(sample >> 8)
-	// 	}
-	// }
-	// return output
+			// simple noise gate
+			if math.Abs(float64(sample)) < ap.noiseThreshold*32767 { // 32767 = max value for 16-bit audio
+				sample = int16(float64(sample) * ap.attenuation)
+			}
+
+			// convert back to bytes
+			output[idx] = byte(sample)
+			output[idx+1] = byte(sample >> 8)
+		}
+	}
+	return output
 }
 
 func (ap *AudioProcessor) SplitChannels(input []byte) ([]byte, []byte) {
